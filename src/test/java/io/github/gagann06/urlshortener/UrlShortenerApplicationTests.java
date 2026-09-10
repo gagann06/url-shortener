e7d0fa3c54615cad2b4ef9c2172011871df81847
Gagan Garcha <gagandeepgarcha11@gmail.com>
2026-09-10 03:24:16 +0100

    Generate Spring Boot skeleton from Initializr

---
+++ src/test/java/io/github/gagann06/urlshortener/UrlShortenerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.gagann06.urlshortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShortenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
